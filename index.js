85fdbaa84e8c6acf1cbd336f9494c68180f8751e
Manzi <manzeyjoseph@gmail.com>
2019-12-12 10:36:58 +0000

    express is being used from here and added some linting

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-// Load the http module to create an http server.
-import http from 'http';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import express from 'express';
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT
-// Configure our HTTP server to respond with Hello World to all requests.
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello  World!');
+const server = express();
+
+const port = process.env.PORT;
+
+server.use(express.static('public'));
+
+server.listen(port, () => {
+  console.info(`Server running at ${port}`);
 });
-
-server.listen(port);
-
-// Put a friendly message on the terminal
-console.log(`Server running at ${port}`);
